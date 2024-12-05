2e36bb5fb53c7905b9a402b45871ef6c8a6e129d
SudhirYadav <sudhirkyadav51@gmail.com>
2024-12-05 17:23:59 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+    let ans = is_even(20);
+    println!("{}", ans);
+}
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
